babd1db1526d99f62b0d03adec61e7bd1562561b
Ivan Cherkas <distor6on@yandex.ru>
2022-03-15 16:47:39 +0300

    feature navbar

---
+++ src/app/components/builder-nav/builder-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-builder-nav',
+  templateUrl: './builder-nav.component.html',
+  styleUrls: ['./builder-nav.component.scss']
+})
+export class BuilderNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
